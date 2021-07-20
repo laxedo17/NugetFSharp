4285f0f01beab4be2a7317c7ab9d226e75875c88
laxedo17 <saracyan78@hotmail.com>
2021-07-20 23:00:15 +0200

    Add project files.

---
+++ NugetFSharp/Library.fs
@@ -0,0 +1,9 @@
+﻿module Libreria
+
+open Newtonsoft.Json
+type Persona = { Nome : string; Idade : int} //definindo un record
+let getPersona() =
+    let texto = """{ "Nome": "Suso", "Idade" : 18 }""" //Texto sample en JSON que se corresponde coa estructura do noso record
+    let persona = JsonConvert.DeserializeObject<Persona>(texto) //Usando Newtonsoft para deserializar o obxeto
+    printfn "O teu nome e %s e tes %d anos" persona.Nome persona.Idade
+    persona
